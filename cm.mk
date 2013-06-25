e7c30eb2f936f1a645c31a0ddf4e47525776cc8a
Shantanu Londhe <shantanulondhe26@gmail.com>
2013-06-25 11:16:55 +0530

    Update cm.mk

--- cm.mk
+++ cm.mk
@@ -1,17 +1,8 @@
-# Prepare for new BootAnimation
-TARGET_BOOTANIMATION_NAME := vertical-240x320
-
-## Specify phone tech before including full_phone
+# Specify phone tech before including full_phone
 $(call inherit-product, vendor/cm/config/gsm.mk)
 
-# Include GSM stuff
-$(call inherit-product, vendor/cm/config/gsm.mk)
-
-# Inherit from those products. Most specific first
-$(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
-
-#$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base.mk)
-$(call inherit-product, device/common/gps/gps_eu_supl.mk)
+# Release name and versioning
+PRODUCT_RELEASE_NAME := totoro
 
 # Inherit some common cyanogenmod stuff.
 $(call inherit-product, vendor/cm/config/common_mini_phone.mk)
@@ -22,13 +13,9 @@ $(call inherit-product, device/samsung/totoro/totoro.mk)
 # Include FM-Radio stuff
 #$(call inherit-product, vendor/cm/products/bcm_fm_radio.mk)
 
+## Device identifier. This must come after all inclusions
 PRODUCT_NAME := cm_totoro
 PRODUCT_BRAND := samsung
 PRODUCT_DEVICE := totoro
 PRODUCT_MODEL := GT-s5360
 PRODUCT_MANUFACTURER := samsung
-PRODUCT_CHARACTERISTICS := phone
-
-# Release name and versioning
-PRODUCT_RELEASE_NAME := totoro
-PRODUCT_VERSION_DEVICE_SPECIFIC := totoro
